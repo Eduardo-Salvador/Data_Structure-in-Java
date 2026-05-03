ccb0831a58dbddd0a8819b29854922f0dbcb98ef
Eduardo-Salvador <eduardo.salvador.gs@gmail.com>
2026-05-02 23:03:17 -0300

    feat: Finish Parcial BST (No Delete Method)

--- src/Trees/BinaryTree/BinaryTree.java
+++ src/Trees/BinaryTree/BinaryTree.java
@@ -1,43 +1,14 @@
-package Trees.BinaryTree;
+package Trees.Tree;
 
-public class BinaryTree<T extends Comparable<T>> {
-    private Node<T> root;
-    private int size;
+public class BinaryTree<T> {
+    protected Node<T> root;
+    protected int size;
 
-    public BinaryTree(){
-        this.root = null;
-    }
-
-    public BinaryTree(T value) {
-        this.root = new Node<T>(value);
-    }
-
-    public BinaryTree(Node<T> root){
-        this.root = root;
-    }
-
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return root == null;
     }
 
-    public int size(){
+    public int size() {
         return size;
-    }
-
-    public void insert(T value) {
-        root = insert(root, value);
-    }
-
-    private Node<T> insert(Node<T> current, T value) {
-        if (current == null) {
-            size++;
-            return new Node<T>(value);
-        }
-        if (value.compareTo(current.getValue()) < 0) {
-            current.setLeft(insert(current.getLeft(), value));
-        } else {
-            current.setRight(insert(current.getRight(), value));
-        }
-        return current;
     }
 }
--- src/Trees/BinaryTree/Node.java
+++ src/Trees/BinaryTree/Node.java
@@ -1,37 +1,19 @@
-package Trees.BinaryTree;
+package Trees.Tree;
 
-public class Node<T extends Comparable<T>> {
+public class Node<T> {
     private T value;
     private Node<T> left;
     private Node<T> right;
 
     public Node(T value) {
         this.value = value;
-        this.left = null;
-        this.right = null;
     }
 
-    public T getValue() {
-        return value;
-    }
+    public T getValue() { return value; }
+    public Node<T> getLeft() { return left; }
+    public Node<T> getRight() { return right; }
 
-    public Node<T> getLeft() {
-        return left;
-    }
-
-    public Node<T> getRight() {
-        return right;
-    }
-
-    public void setValue(T value) {
-        this.value = value;
-    }
-
-    public void setLeft(Node<T> left) {
-        this.left = left;
-    }
-
-    public void setRight(Node<T> right) {
-        this.right = right;
-    }
+    public void setValue(T value) { this.value = value; }
+    public void setLeft(Node<T> left) { this.left = left; }
+    public void setRight(Node<T> right) { this.right = right; }
 }
